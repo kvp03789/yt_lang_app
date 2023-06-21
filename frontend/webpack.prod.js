a5c5e3be2b41af2d25af3bc69d919d57e613ad4b
Clay Nichols <clay.nichols@gmail.com>
2023-06-21 02:52:30 -0500

    change dist folder name to public

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
     mode: "production",
     output: {
         filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "public")
     },
     plugins: [new CleanWebpackPlugin() ]
 })
